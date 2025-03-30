f79f0fa86017029bc99c67be2b7c035d5f2a6454
JosephTao_FCU <josttw@gmail.com>
2025-03-30 18:14:45 +0800

    Update Lighthouse CI configuration to reduce number of runs and adjust performance assertion level

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,14 +3,14 @@ module.exports = {
   ci: {
     collect: {
       url: ['https://yolo-cat.github.io/1132-WEB-FreeCSS-Performance/'],
-      numberOfRuns: 3,
+      numberOfRuns: 1,
       settings: {
         chromeFlags: '--no-sandbox --headless',
       },
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:performance': ['warn', { minScore: 0.9 }],
         'categories:accessibility': ['warn', { minScore: 0.9 }],
         'categories:best-practices': ['warn', { minScore: 0.9 }],
         'categories:seo': ['warn', { minScore: 0.9 }],
