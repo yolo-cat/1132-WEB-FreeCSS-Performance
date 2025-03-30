f818b4f6fd6b8acee9c8ad0992aa09e0f6873f58
JosephTao_FCU <josttw@gmail.com>
2025-03-30 16:23:10 +0800

    Add Lighthouse CI configuration file

---
+++ .lighthouserc.js
@@ -0,0 +1,25 @@
+// .lighthouserc.js
+module.exports = {
+  ci: {
+    collect: {
+      url: ['https://yolo-cat.github.io/1132-WEB-FreeCSS-Performance/'],
+      numberOfRuns: 3,
+      settings: {
+        chromeFlags: '--no-sandbox --headless',
+      },
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'filesystem',
+      outputDir: './lhci-report',
+      reportFilenamePattern: 'report-%%DATETIME%%.html',
+    },
+  },
+};
